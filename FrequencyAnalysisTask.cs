becc3eae6c7070967e6664e3bb11d3ee655e90d9
Макс <yashanty@gmail.com>
2020-12-20 12:59:33 +0300

    оставляю временно

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        static Dictionary<string, int> FrequentValue(List<List<string>> text, string wordCount)
+        {
+            var wordsCountDict= new Dictionary<string,int>();
+            foreach (var list in text)
+            {
+                if(list.Count>1)
+                {
+                    for (var i = 0; i < list.Count - 1; i++)
+                    {
+                        if (list[i] == wordCount)
+                        {
+                            if (!wordsCountDict.ContainsKey(list[i + 1]))
+                                wordsCountDict.Add(list[i + 1], 1);
+                            else wordsCountDict[list[i + 1]]++;
+                        }
+                    }
+                    for (var i = 0; i < list.Count - 2; i++)
+                    {
+                        if (list[i] + " " + list[i + 1] == wordCount)
+                        {
+                            if (!wordsCountDict.ContainsKey(list[i + 2]))
+                                wordsCountDict.Add(list[i + 2], 1);
+                            else wordsCountDict[list[i + 2]]++;
+                        }
+                    }
+                }
+            }
+            return wordsCountDict;
+        }
+
+        //static Dictionary<string, Dictionary<string, int>> AllFrecuentDictionary(List<List<string>> text)
+        //{
+        //    var dictionariAll = new Dictionary<string, Dictionary<string, int>>();
+        //    foreach (var list in text)
+        //    {
+        //        if (list.Count > 1)
+        //        {
+        //            for (var i = 0; i < list.Count - 1; i++)
+        //            {
+        //                if (!dictionariAll.ContainsKey(list[i]))
+        //                {
+        //                    var valMax = 0;
+        //                    foreach (var pair in FrequentValue(text, list[i].ToString()))
+        //                    {
+        //                        if (pair.Value > valMax)
+
+        //                    }
+        //                }
+
+
+
+
+        //                        dictionariAll.Add(list[i], FrequentValue(text, list[i].ToString()));
+        //            }
+        //            for (var i = 0; i < list.Count - 2; i++)
+        //            {
+        //                if (!dictionariAll.ContainsKey(list[i] + " " + list[i + 1]))
+        //                {
+        //                    dictionariAll.Add(list[i] + " " + list[i + 1], FrequentValue(text, list[i] + " " + list[i + 1]));
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return dictionariAll;
+        //}
+
+        //static string WordValMaxLexMin(KeyValuePair<string, int> pair)
+        //{
+        //    var valMax = 0;
+        //    var wordMax = "";
+        //    if (pair.Value > valMax)
+        //    {
+        //        valMax = pair.Value;
+        //        wordMax = pair.Key;
+        //    }
+        //    else if (pair.Value == valMax && string.CompareOrdinal(pair.Key, wordMax) < 0)
+        //    {
+        //        wordMax = pair.Key;
+        //    }
+        //    return wordMax;
+        //}
+
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        {
+            var result = new Dictionary<string, string>();
+
+            foreach (var list in text)
+            {
+                if (list.Count > 1)
+                {
+                    for (var i = 0; i < list.Count - 1; i++)
+                    {
+                        if (!result.ContainsKey(list[i]))
+                        {
+                            var valMax = 0;
+                            string wordMax="";
+                            foreach (var pair in FrequentValue(text, list[i].ToString()))
+                            {
+                                //wordMax = WordValMaxLexMin(pair);
+                                if (pair.Value > valMax)
+                                {
+                                    valMax = pair.Value;
+                                    wordMax = pair.Key;
+                                }
+                                else if (pair.Value == valMax && string.CompareOrdinal(pair.Key, wordMax) < 0)
+                                {
+                                    wordMax = pair.Key;
+                                }
+                            }
+                            result.Add(list[i],wordMax);
+                        }
+                    }
+                    
+                    for (var i = 0; i < list.Count - 2; i++)
+                    {
+                        if (!result.ContainsKey(list[i] + " " + list[i + 1]))
+                        {
+                            var valMax = 0;
+                            string wordMax = "";
+                            foreach (var pair in FrequentValue(text, list[i] + " " + list[i + 1]))
+                            {
+                                //wordMax = WordValMaxLexMin(pair);
+                                if (pair.Value > valMax)
+                                {
+                                    valMax = pair.Value;
+                                    wordMax = pair.Key;
+                                }
+                                else if (pair.Value == valMax && string.CompareOrdinal(pair.Key, wordMax) < 0)
+                                {
+                                    wordMax = pair.Key;
+                                }
+                            }
+                            result.Add(list[i] + " " + list[i + 1], wordMax);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+   }
+}
---
+++ SentencesParserTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+       static List<string> WordsList(string sentenceL)  //метод типа List<string> принимает предложение в нижнем регистре
+        {
+            var separatorsArr = new List<char>();       //заводим новый лист типа char для сепараторов, по которым будет разито предложение на отдельные слова
+            foreach (var symbol in sentenceL)           //для каждого символа в предложении находим любой символ не являющийся буквой или символом апострофа
+            {
+                if (!(char.IsLetter(symbol) || symbol == '\'') && !separatorsArr.Contains(symbol))// а так же добавляем условие, чтобы не было дубликатов
+                    separatorsArr.Add(symbol);          //добавляем в лист символы для сепаратора(разделителя на слова) из предложения
+            }
+            //var wordArr = sentenceL.Split(separatorsArr.ToArray(), StringSplitOptions.RemoveEmptyEntries);//создаем массив для слов и разбиваем                                                           предложение используя лист сепараторов, переведенных в массив(должен быть массив, а не лист, а в лист                                               удобно добавлять без задавания конечной длинны)
+            var wordList = new List<string>(sentenceL.Split(separatorsArr.ToArray(), StringSplitOptions.RemoveEmptyEntries));
+            //wordList.AddRange(sentenceL.Split(separatorsArr.ToArray(), StringSplitOptions.RemoveEmptyEntries));//кладем массив слов в лист
+            return wordList;                            //возвращаем лист слов
+       }
+
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();//заводится лист листов
+            var sentences = new List<string>();          //объявляется лист для отдельных предложений
+            sentences.AddRange(text.Split(new char[]
+            {
+                '.', '!', '?', ';', ':', '(', ')'
+            }, StringSplitOptions.RemoveEmptyEntries)); //в лист для предложений кладется весь разбитый(по символам) text на массив предложений типа string
+            foreach (var sentence in sentences)         //для каждого элемента массива - предложения - из всех предложений
+            {
+                var sentenceLower = sentence.ToLower(); //заводим новую переменную для предложения - переводим все символы предложения в нижний регистр
+                if(WordsList(sentenceLower).Count!=0)   //берем предложение, переведенное в нижний регистр, и используем метод, который возвращает лист                                                   слов.Исключаем из последовательности слов пустые значения.
+                    sentencesList.Add(WordsList(sentenceLower));//добавляем в лист (листов) последовательность(лист) слов из каждого предложения
+            }
+            return sentencesList;                       //возвращаем лист листов - лист предложений, каждое предложение разбито на лист слов
+        }
+    }
+}
